3f03cb9ed322b4ec95aec3c54ad9b5a74a7aebdb
KelseyRocco <kelseyroccoh@gmail.com>
2022-06-27 15:53:37 -0400

    display profile info on page

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 //GLOBAL VARIABLES
 
 //profile info section
-const profileInfo = document.querySelector(".overview");
+const overview = document.querySelector(".overview");
 const username = "KelseyRocco";
-
 //FUNCTIONS
 
 //Fetch GitHub profile info
@@ -11,6 +10,22 @@ const gitHubProfile = async function () {
   const reqProfile = await fetch(`https://api.github.com/users/${username}`);
   const data = await reqProfile.json();
   console.log(data);
+  userInfo(data);
 };
 
 gitHubProfile();
+
+const userInfo = function (data) {
+  const div = document.createElement("div");
+  div.innerHTML = `
+    <figure>
+        <img alt="user avatar" src=${data.avatar_url} />
+    </figure>
+    <div>
+        <p><strong>Name:</strong> ${data.name}</p>
+        <p><strong>Bio:</strong> ${data.bio}</p>
+        <p><strong>Location:</strong> ${data.location}</p>
+        <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    </div>`;
+  overview.append(div);
+};
